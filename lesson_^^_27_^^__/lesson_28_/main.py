9b35f34ae0098acad4446b136c358cca79287367
mehrishka177 <mehri@hajiagali.com>
2024-11-15 19:27:07 +0400

    hlsdf

--- lesson_^^_27_^^__/lesson_28_/main.py
+++ lesson_^^_27_^^__/lesson_28_/main.py
@@ -16,3 +16,17 @@ modelX  = Vehicle(240, 18)
 
 print("Model Max Speed:",modelX.max_speed)
 print("model Mileage:", modelX.mileage)
+
+class Parrot:
+    
+    species = "bird"
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    blu = Parrot("Blu", 10)
+    woo = Parrot("Woo is also {}".format( blu.name, blu.age))
+    
+    print("{} is {} years old".format( blu.name, blu.age))
+    print("{} is {} years old".formt( woo.name, woo.age))
