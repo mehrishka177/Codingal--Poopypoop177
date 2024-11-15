213b17b5e8d59d0430be5c125db4c5caf18db507
mehrishka177 <mehri@hajiagali.com>
2024-11-15 19:16:05 +0400

    jgn;a/

--- lesson_^^_27_^^__/lesson_28_/main.py
+++ lesson_^^_27_^^__/lesson_28_/main.py
@@ -3,3 +3,16 @@ class student:
     print("Hi i am a student of grade", grade)
     
 ob = student()
+
+class Vehicle:
+    
+    def __int__(self, max_speed, mileage):
+        
+        
+        self.max_speed = max_speed
+        self.mileage = mileage    
+          
+modelX  = Vehicle(240, 18)
+
+print("Model Max Speed:",modelX.max_speed)
+print("model Mileage:", modelX.mileage)
